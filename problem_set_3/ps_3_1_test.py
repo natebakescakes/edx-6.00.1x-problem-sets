13a9dfe2c0b2a3d17a78e3f7c636d4bb0be1e245
Nathan Khoo <nathanspacekhoo@gmail.com>
2016-02-02 17:28:31 +0800

    first commit!

---
+++ problem_set_3/ps_3_1_test.py
@@ -0,0 +1,21 @@
+import unittest
+import math
+
+import ps_3_1
+
+class TestPS_3_1(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertAlmostEqual(ps_3_1.radiationExposure(0, 5, 1), 39.10318784326239, places=2)
+
+    def test_case_2(self):
+        self.assertAlmostEqual(ps_3_1.radiationExposure(5, 11, 1), 22.94241041057671, places=2)
+
+    def test_case_3(self):
+        self.assertAlmostEqual(ps_3_1.radiationExposure(0, 11, 1), 62.0455982538, places=2)
+
+    def test_case_4(self):
+        self.assertAlmostEqual(ps_3_1.radiationExposure(40, 100, 1.5), 0.434612356115, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
